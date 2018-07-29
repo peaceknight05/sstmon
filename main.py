4a6930c93a712e21e85f508dced41c1ed8e14b8c
Jonathan Tan Jiayi <jonathantanatlol@gmail.com>
2018-07-29 15:02:25 +0800

    balance (with bug fix)

--- main.py
+++ main.py
@@ -15,8 +15,14 @@ e.nullify()
 while((p.getPlayerHealth() > 0) & (a.getTrainerHealth() > 0)):
     e.turn("player")
     p.makeMove()
+    if(a.getTrainerHealth() <= 0):
+        break
     e.turn("trainer")
     a.makeMove()
+    if(p.getPlayerHealth() <= 0):
+        break
     e.updateEffect()
+    if((p.getPlayerHealth() <= 0) | (a.getTrainerHealth() <= 0)):
+        break
     print("Your health: " + str(p.getPlayerHealth()))
     print("Opponent's health: " + str(a.getTrainerHealth()))
